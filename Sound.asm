fdf90c2f3481cff1a1315e698a26718d37a76aaf
Brady Lewis <lawlmomotx@gmail.com>
2023-04-25 19:06:50 -0500

    added sounds

--- Sound.asm
+++ Sound.asm
@@ -1,4 +1,21 @@
-BadSound:
+.globl bad_sound good_sound
+
+.text
+j good_sound
+bad_sound:
+	li $a0 64
+	li $a1 100
+	li $a2 1
+	li $a3 127
+	li $v0 31
+	syscall
+	li $a0 65
+	li $a1 100
+	li $a2 1
+	li $a3 127
+	li $v0 33
+	syscall
+	
 	li $a0 64
 	li $a1 100
 	li $a2 1
@@ -12,3 +29,11 @@ BadSound:
 	li $v0 33
 	syscall
 	jr $ra
+	
+good_sound:
+	li $a0 70
+	li $a1 100
+	li $a2 16
+	li $a3 127
+	li $v0 31
+	syscall
