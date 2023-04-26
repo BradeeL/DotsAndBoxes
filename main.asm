ea91f18bc46ce55c804e564ad79ec8befe436fb2
bluebrowney <121346533+bluebrowney@users.noreply.github.com>
2023-04-26 14:30:28 -0500

    Update main.asm
    Comments and taking out sound

--- main.asm
+++ main.asm
@@ -4,16 +4,20 @@
 #--DESCRIPTION--
 #
 # In this file, the game values are stored in memory (initialized) and put into a 
-# loop which runs the game in 3 stages: Input, Update, Render. These subroutines are
+# loop which runs the game in 4 steps: Render, Request, Update. and AIExec These subroutines are
 # defined in other files, but are put together in main.
 
 #PRE-DEFINED CONSTANTS
 # define PLAYER_TURN 1
 # define AI_TURN 0
+# define (return from Update) SCORED_POINT 1
+# define (return from Update) !SCORED_POINT 0
+#define GAMEOVER 1
+#define !GAMEOVER 0
 
 #PRE-DEFINED REGISTERS
-# define $s7 : player points
-# define $s6 : AI points
+# define $s7 : AI points
+# define $s6 : Player points
 # define $s5 : max points
 # define $s4 : game over flag
 
@@ -21,19 +25,23 @@
 
 .globl main exit_game
 
-p_winner: .asciiz "The player wne"
-a_winner: .asciiz "The AI one"
+#PROMPTS
+p_winner: .asciiz "Congratulations, You won!"
+a_winner: .asciiz "HAHAHAHAHHAH, you lost I won"
+
 
 .text
 
 #Runs main loop of the program 
-main:		addi $s4, $zero, 0
+main:		addi $s4, $zero, 0		#Initializing pre-defined registers (gameover flag, maxpoints)
 		addi $s5, $zero, 35
-	
+
+
+
 
 main_Loop:	jal Render			#Print board to console
 		
-		jal Request			#Request user input with validity handling
+		jal Request			#Request user input with validity handling returns (memory deplacement and dir of coordinate)
 		
 		move $a0, $v0			#Passes return values of Request(mem_displacement, direction, turn)
 		move $a1, $v1			#to arguments for Update.
@@ -41,25 +49,39 @@ main_Loop:	jal Render			#Print board to console
 		
 		jal Update			#Function call update
 		
-		#LOOPING
+#TURN HANDLING
 		bne $zero, $s4, exit_game	#If game is not over(0) --> loop
 		bne $v0, $zero, main_Loop	#Loop back if player scored points
-		
-		jal good_sound
 
-AI_Loop:
 
-		jal Render			#Render User move
-		jal AIExec			#AI Turn
-	
-		#LOOPING
+
+
+AI_Loop:	jal AIExec			#AI Turn
+
+#TURN HANDLING
 		bne $zero, $s4, exit_game	#If game is not over(0) --> loop
-		bne $v0, $zero, AI_Loop	#Loop back if player scored points
+		bne $v0, $zero, AI_Loop	#Loop back if AI scored points
 	
+	
+#LOOPING
 		j main_Loop			#Loop back to Render function call
+		
+
 	
-		#GAME FINISHED
+#GAME FINISHED
 exit_game:	
-		jal exit_sound
+		li $v0 4			#Set syscall code to print string
+		
+		slt $t0, $s7, $s6		#Store if user scored more point ? 1 : 0
+		
+		la $a0, p_winner		#Predict Player win and set Player win prompt
+		
+		bne $t0, $zero, PlayerW	#If player points < AI points --> print and end game
+				
+		la $a0, a_winner		#Set AI win prompt
+
+
+PlayerW:	syscall				#Print winner prompt
+		
 		li $v0 10 			#exit program
 		syscall
